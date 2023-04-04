906464490e9a01a5800c7b51b97b7c94578bdf3b
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-04-04 18:18:15 +0530

    comments model, controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   post "toggle_like", to: "likes#toggle_like", as: :toggle_like
+  resources :comments, only: %i[create destroy]
 end
