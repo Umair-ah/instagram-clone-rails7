58da08833aaa7d45b051eb0bc320822a58c5f7dd
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-04-05 17:08:06 +0530

    designed user's profile header

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   post "toggle_like", to: "likes#toggle_like", as: :toggle_like
+  resources :users, only: [:show]
   resources :comments, only: %i[create destroy]
 end
