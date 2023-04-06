11abf762e556acf8c400c7fc7cffe3c4f2cbd95d
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-04-06 18:01:05 +0530

    added Following Model thorugh many-to-many rs

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ApplicationRecord
 
   has_many :likes
   has_many :comments
+
+
+  has_many :follow_requests, ->{where(accepted: false)}, class_name: "Follow", foreign_key: "followed_id"
+  has_many :accepted_recieved_requests, ->{where(accepted: true)}, class_name: "Follow", foreign_key: "followed_id"
+
+  has_many :accepted_sent_requests, ->{where(accepted: true)}, class_name: "Follow", foreign_key: "followed_id"
+
+  has_many :followers, through: :accepted_recieved_requests, source: :follower
+  has_many :followings, through: :accepted_sent_requests, source: :followed
 end
